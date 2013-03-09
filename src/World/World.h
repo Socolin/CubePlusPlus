58acca34ec51f051ac3eea815ae12606946ad881
Socolin <devnull@localhost>
2013-03-09 18:46:23 +0100

    Split world header and world inline methods

--- src/World/World.h
+++ src/World/World.h
@@ -26,116 +26,37 @@ class World
 public:
     World();
     virtual ~World();
+
     void UpdateTick();
+
     void AddEntity(Entity* entity);
     void AddPlayer(EntityPlayer* entity);
+
     void RemoveEntity(Entity* entity);
     void RemovePlayer(EntityPlayer* entity);
 
+    inline Chunk* GetChunk(int x, int z);
+    inline Chunk* GetChunkIfLoaded(int x, int z);
 
-    inline Chunk* GetChunk(int x, int z)
-    {
-        auto it = chunkMap.find(CHUNK_KEY(x,z));
-        if (it == chunkMap.end())
-        {
-            Chunk* chunk = LoadChunk(x, z);
-            chunkMap[CHUNK_KEY(x,z)] = chunk;
-            return chunk;
-        }
-        return it->second;
-    }
+    inline VirtualChunk* GetVirtualChunk(int x, int z);
+    inline VirtualChunk* GetVirtualChunkIfLoaded(int x, int z);
 
-    inline Chunk* GetChunkIfLoaded(int x, int z)
-    {
-        auto it = chunkMap.find(CHUNK_KEY(x,z));
-        if (it == chunkMap.end())
-        {
-            return NULL;
-        }
-        return it->second;
-    }
+    inline VirtualSmallChunk* GetVirtualSmallChunk(int x, int z);
+    inline VirtualSmallChunk* GetVirtualSmallChunkIfLoaded(int x, int z);
 
-    inline VirtualChunk* GetVirtualChunk(int x, int z)
-    {
-        auto it = virtualChunkMap.find(CHUNK_KEY(x,z));
-        if (it == virtualChunkMap.end())
-        {
-            VirtualChunk* vChunk = CreateVirtualChunk(x, z);
-            virtualChunkMap[CHUNK_KEY(x,z)] = vChunk;
-            return vChunk;
-        }
-        return it->second;
-    }
+    inline i_block GetBlockId(int x, i_height y, int z);
+    inline i_data GetBlockData(int x, i_height y, int z);
 
-    inline VirtualChunk* GetVirtualChunkIfLoaded(int x, int z)
-    {
-        auto it = virtualChunkMap.find(CHUNK_KEY(x,z));
-        if (it == virtualChunkMap.end())
-        {
-            return nullptr;
-        }
-        return it->second;
-    }
+    inline s_block_data GetBlockIdAndData(int x, i_height y, int z);
 
-    inline VirtualSmallChunk* GetVirtualSmallChunk(int x, int z)
-    {
-        auto it = virtualSmallChunkMap.find(CHUNK_KEY(x,z));
-        if (it == virtualSmallChunkMap.end())
-        {
-            VirtualSmallChunk* vChunk = CreateVirtualSmallChunk(x, z);
-            virtualSmallChunkMap[CHUNK_KEY(x,z)] = vChunk;
-            return vChunk;
-        }
-        return it->second;
-    }
-
-    inline VirtualSmallChunk* GetVirtualSmallChunkIfLoaded(int x, int z)
-    {
-        auto it = virtualSmallChunkMap.find(CHUNK_KEY(x,z));
-        if (it == virtualSmallChunkMap.end())
-        {
-            return nullptr;
-        }
-        return it->second;
-    }
-
-    inline i_block GetBlockId(int x, i_height y, int z)
-    {
-        Chunk* chunk = GetChunkIfLoaded(x >> 4, z >> 4);
-        if (chunk == nullptr)
-            return 0;
-        return chunk->getBlockAt(x & 0xf, y, z & 0xf);
-    }
-
-    inline i_data GetBlockData(int x, i_height y, int z)
-    {
-        Chunk* chunk = GetChunkIfLoaded(x >> 4, z >> 4);
-        if (chunk == nullptr)
-            return 0;
-        return chunk->getDataAt(x & 0xf, y, z & 0xf);
-    }
-
-    inline s_block_data GetBlockIdAndData(int x, i_height y, int z)
-    {
-        Chunk* chunk = GetChunkIfLoaded(x >> 4, z >> 4);
-        s_block_data block_data;
-        if (chunk != nullptr)
-        {
-            block_data.blockId = chunk->getBlockAt(x & 0xf, y, z & 0xf);
-            block_data.blockData = chunk->getDataAt(x & 0xf, y, z & 0xf);
-        }
-        else
-        {
-            block_data.blockId = 0;
-        }
-        return block_data;
-    }
     void PlaceBlock(int x, i_height y, int z, i_block blockId, i_data blockData);
     void SendPacketToPlayerInWorld(const Network::NetworkPacket& packet) const;
 
     void Unload();
     void RequestChunk(EntityPlayer* player, std::pair<int, int>);
+
     int getViewDistance();
+
     void PlaySound(double x, double y, double z, const std::wstring& soundName, float volume, char modifier, unsigned char distanceChunk);
     void PlayBlockAction(int x, short y, int z, char type, char modifier, i_block blockId, char distanceChunk);
 private:
@@ -155,4 +76,7 @@ private:
 };
 
 } /* namespace World */
+
+#include "World.hxx"
+
 #endif /* WORLD_H_ */
---
+++ src/World/World.hxx
@@ -0,0 +1,106 @@
+#ifndef WORLD_HXX_
+#define WORLD_HXX_
+
+namespace World
+{
+
+inline Chunk* World::GetChunk(int x, int z)
+{
+    auto it = chunkMap.find(CHUNK_KEY(x,z));
+    if (it == chunkMap.end())
+    {
+        Chunk* chunk = LoadChunk(x, z);
+        chunkMap[CHUNK_KEY(x,z)] = chunk;
+        return chunk;
+    }
+    return it->second;
+}
+
+inline Chunk* World::GetChunkIfLoaded(int x, int z)
+{
+    auto it = chunkMap.find(CHUNK_KEY(x,z));
+    if (it == chunkMap.end())
+    {
+        return NULL;
+    }
+    return it->second;
+}
+
+inline VirtualChunk* World::GetVirtualChunk(int x, int z)
+{
+    auto it = virtualChunkMap.find(CHUNK_KEY(x,z));
+    if (it == virtualChunkMap.end())
+    {
+        VirtualChunk* vChunk = CreateVirtualChunk(x, z);
+        virtualChunkMap[CHUNK_KEY(x,z)] = vChunk;
+        return vChunk;
+    }
+    return it->second;
+}
+
+inline VirtualChunk* World::GetVirtualChunkIfLoaded(int x, int z)
+{
+    auto it = virtualChunkMap.find(CHUNK_KEY(x,z));
+    if (it == virtualChunkMap.end())
+    {
+        return nullptr;
+    }
+    return it->second;
+}
+
+inline VirtualSmallChunk* World::GetVirtualSmallChunk(int x, int z)
+{
+    auto it = virtualSmallChunkMap.find(CHUNK_KEY(x,z));
+    if (it == virtualSmallChunkMap.end())
+    {
+        VirtualSmallChunk* vChunk = CreateVirtualSmallChunk(x, z);
+        virtualSmallChunkMap[CHUNK_KEY(x,z)] = vChunk;
+        return vChunk;
+    }
+    return it->second;
+}
+
+inline VirtualSmallChunk* World::GetVirtualSmallChunkIfLoaded(int x, int z)
+{
+    auto it = virtualSmallChunkMap.find(CHUNK_KEY(x,z));
+    if (it == virtualSmallChunkMap.end())
+    {
+        return nullptr;
+    }
+    return it->second;
+}
+
+inline i_block World::GetBlockId(int x, i_height y, int z)
+{
+    Chunk* chunk = GetChunkIfLoaded(x >> 4, z >> 4);
+    if (chunk == nullptr)
+        return 0;
+    return chunk->getBlockAt(x & 0xf, y, z & 0xf);
+}
+
+inline i_data World::GetBlockData(int x, i_height y, int z)
+{
+    Chunk* chunk = GetChunkIfLoaded(x >> 4, z >> 4);
+    if (chunk == nullptr)
+        return 0;
+    return chunk->getDataAt(x & 0xf, y, z & 0xf);
+}
+
+inline s_block_data World::GetBlockIdAndData(int x, i_height y, int z)
+{
+    Chunk* chunk = GetChunkIfLoaded(x >> 4, z >> 4);
+    s_block_data block_data;
+    if (chunk != nullptr)
+    {
+        block_data.blockId = chunk->getBlockAt(x & 0xf, y, z & 0xf);
+        block_data.blockData = chunk->getDataAt(x & 0xf, y, z & 0xf);
+    }
+    else
+    {
+        block_data.blockId = 0;
+    }
+    return block_data;
+}
+}
+
+#endif
