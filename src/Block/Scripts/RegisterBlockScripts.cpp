f0a31fedd048bb8c266ba1e5aaa6e979970e5aeb
Socolin <devnull@localhost>
2013-03-09 19:07:06 +0100

    Clean

--- src/Block/Scripts/RegisterBlockScripts.cpp
+++ src/Block/Scripts/RegisterBlockScripts.cpp
@@ -2,12 +2,12 @@
 
 #include "Basics/BlockDoorScript.h"
 #include "Basics/BlockJackOLanternScript.h"
-#include "Basics/BlockNoteScript.h"
-#include "Basics/StairScript.h"
-#include "Basics/BlockLeverScript.h"
-#include "Basics/BlockWoodScript.h"
-#include "Basics/TorchScript.h"
 #include "Basics/BlockJukeboxScript.h"
+#include "Basics/BlockLeverScript.h"
+#include "Basics/BlockNoteScript.h"
+#include "Basics/BlockWoodScript.h"
+#include "Basics/StairScript.h"
+#include "Basics/TorchScript.h"
 
 namespace Scripting
 {
@@ -15,11 +15,11 @@ void RegisterBlockScript()
 {
     new BlockDoorScript();
     new BlockJackOLanternScript();
+	new BlockJukeboxScript();
+    new BlockLeverScript();
     new BlockNoteScript();
     new BlockWoodScript();
     new StairScript();
-    new BlockLeverScript();
 	new TorchScript();
-	new BlockJukeboxScript();
 }
 }
