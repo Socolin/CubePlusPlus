c6d046c0ad8ead454c9b0f5f4a5d58c91048a779
Yann291 <yann291@gmail.com>
2013-10-30 11:03:31 +0100

    Change Log message when config file is checked (fprintf instead of Logger)

--- src/Config/Config.cpp
+++ src/Config/Config.cpp
@@ -30,30 +30,30 @@ void Config::Init()
 {
     try
     {
-        LOG_INFO << "Loading Config File..." << std::endl;
+        fprintf(stdout, "Loading Config File \"%s\" ...\n", fileName.c_str());
         serverConfig.readFile(fileName.c_str());
     }
     catch(const libconfig::FileIOException &fioex)
     {
-        LOG_ERROR << "Can't read file \"" << fileName << "\". Abording." << std::endl;
+        fprintf(stderr, "Can't read file \" %s \". Abording.\n", fileName.c_str());
         exit(EXIT_FAILURE);
     }
     catch(const libconfig::ParseException &pex)
     {
-        LOG_ERROR << "Parse error at " << pex.getFile() << ":" << pex.getLine() << " - " << pex.getError() << std::endl;
+        fprintf(stderr, "Parse error at %s : %d - %s\n", pex.getFile(), pex.getLine(), pex.getError());
         exit(EXIT_FAILURE);
     }
     serverConfig.lookupValue("server.general.log-dir", logDir);
     serverConfig.lookupValue("server.world.chunk.chunk-sent-per-tick", chunkSentPerTick);
     if (chunkSentPerTick < 1 || chunkSentPerTick > 16)
     {
-        LOG_ERROR << "Invalid chunkSentPerTick value: " << chunkSentPerTick << ", value must be between 1 and 16" << std::endl;
+        fprintf(stderr, "Invalid chunkSentPerTick value: %d, value must be between 1 and 16\n", chunkSentPerTick);
         chunkSentPerTick = 16;
     }
     serverConfig.lookupValue("server.general.gamemode", gamemode);
     if(gamemode < 0 || gamemode > 2)
     {
-        LOG_ERROR << "Invalid gamemode value: " << gamemode << ", gamemode 1 used (survival)" << std::endl;
+        fprintf(stderr, "Invalid gamemode value: %d, gamemode 1 used (survival)\n", gamemode);
         gamemode = 1;
     }
 }
