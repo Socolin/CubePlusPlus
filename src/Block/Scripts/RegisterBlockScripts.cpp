eaff2bfa48af2f24f9c9b55b4c5a6a07ad927024
Socolin <devnull@localhost>
2013-11-30 23:16:43 +0100

    Improve script to generate script

--- src/Block/Scripts/RegisterBlockScripts.cpp
+++ src/Block/Scripts/RegisterBlockScripts.cpp
@@ -21,6 +21,7 @@
 #include "Basics/BlockSignGroundScript.h"
 #include "Basics/BlockSignWallScript.h"
 #include "Basics/BlockStairScript.h"
+#include "Basics/BlockTestScript.h"
 #include "Basics/BlockTorchScript.h"
 #include "Basics/BlockWoodScript.h"
 
@@ -49,6 +50,7 @@ void RegisterBlockScript()
     new BlockSignGroundScript();
     new BlockSignWallScript();
     new BlockStairScript();
+    new BlockTestScript();
     new BlockTorchScript();
     new BlockWoodScript();
 }
